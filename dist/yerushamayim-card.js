25d314959c684126f1377d0440b639d963b7bc09
chilikla <chilikla@users.noreply.github.com>
2024-10-18 00:36:41 +0000

    commit dist

--- dist/yerushamayim-card.js
+++ dist/yerushamayim-card.js
@@ -10,8 +10,6 @@ function loadCSS(url) {
 
 loadCSS("https://fonts.googleapis.com/css2?family=Rubik&display=swap");
 
-const ENTITY = "sensor.yerushamayim";
-
 class YerushamayimCard extends LitElement {
   static get properties() {
     return {
@@ -21,7 +19,8 @@ class YerushamayimCard extends LitElement {
   }
 
   render() {    
-    const state = this.hass.states[ENTITY];
+    const entityId = this.config.entity;
+    const state = this.hass.states[entityId];
     const stateStr = state ? state.state : 'unavailable';
     const logUrl = this.hass.states['sun.sun'].state === 'below_horizon' ? 'https://www.02ws.co.il/img/logo_night.png' : 'https://www.02ws.co.il/img/logo.png';
 
@@ -92,6 +91,9 @@ class YerushamayimCard extends LitElement {
   }
 
   setConfig(config) {
+    if (!config.entity) {
+      throw new Error('You need to define an entity');
+    }
     this.config = config;
   }
 
