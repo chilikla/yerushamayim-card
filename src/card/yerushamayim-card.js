0b937e5a9610f9bd73539ed1a7d5995d2772d92b
chilikla <chilikla@gmail.com>
2024-12-01 02:02:14 +0200

    fix: yesteraday line #3

--- src/card/yerushamayim-card.js
+++ src/card/yerushamayim-card.js
@@ -225,7 +225,7 @@ class YerushamayimCard extends LitElement {
                     </div>
                     <div class="yesterday-status">
                       <div>
-                        <img style="height: 30px;" src="${this.lastDayState.cloth_icon}" />
+                        <img style="height: 30px; padding-block: 8px;" src="${this.lastDayState.cloth_icon}" />
                       </div>
                       <div>
                         ${this.lastDayState.status}
@@ -330,14 +330,14 @@ class YerushamayimCard extends LitElement {
         align-items: flex-start;
       }
       .yesterday-temperature {
-        height: 55px;
+        height: 76px;
         display: flex;
         flex-direction: column;
         justify-content: space-between;
         align-items: flex-start;
       }
       .yesterday-status {
-        height: 55px;
+        height: 76px;
         display: flex;
         flex-direction: column;
         justify-content: space-between;
