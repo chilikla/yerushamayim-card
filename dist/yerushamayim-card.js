e32171a09458cca594d9a83bb6e282cbedea94fb
chilikla <13035026+chilikla@users.noreply.github.com>
2025-12-04 00:22:27 +0000

    commit dist

--- dist/yerushamayim-card.js
+++ dist/yerushamayim-card.js
@@ -681,7 +681,7 @@ class YerushamayimCard extends LitElement {
       .columns-container {
         display: flex;
         flex-direction: row;
-        align-items: flex-start;
+        align-items: stretch;
         gap: 16px;
         flex: 1;
       }
@@ -726,7 +726,7 @@ class YerushamayimCard extends LitElement {
       #forecast-icons {
         display: flex;
         flex-direction: column;
-        align-items: center;
+        align-items: flex-start;
         gap: 5px;
       }
       .forecast-icon {
